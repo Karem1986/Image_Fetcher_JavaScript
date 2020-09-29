2e6208be6ef67baab9eb9b4212d932d3fb8b4da7
Karem1986 <karem.ortiz@outlook.com>
2020-09-29 12:28:50 +0200

    Save image urls to local storage

--- image.js
+++ image.js
@@ -10,17 +10,40 @@
 //3. Error handling: by making the local storage a promise,
 //error can be handled by a catch feature, in case there are no images to download.
 
+//Local Storage--> LS
+
+class Store {
+  //1. We get the image's URLs
+  static getUrls() {
+    let urls = [];
+    if (localStorage.getItem("url") === null) {
+      urls = [];
+    } else {
+      urls = JSON.parse(localStorage.getItem("urls"));
+    }
+    return urls;
+  }
+  //2. After, we add them to LS
+  static addImagesLocalStorage(url) {
+    const myUrls = Store.getUrls();
+    myUrls.push(url);
+    localStorage.setItem("urls", JSON.stringify(myUrls));
+  }
+}
+
 function imageFetcher(url) {
-  // console.log("image fetcher");
+  // console.log("image fetcher"); //tests the image fetcher
+  //1. Define an empty array to place our images
   const downloadImages = [];
+  //2. Iterate
   for (let i = 0; i < url.length; i++) {
     const element = url[i];
-    const myImages = downloadImages.push("url"); //push the urls to the empty []
+    const myImages = downloadImages.push("url"); //Push the urls to the empty []
   }
+
+  Store.addImagesLocalStorage(url);
 
   return downloadImages;
 }
 
 console.log(imageFetcher("url"));
-
-//Local Storage
